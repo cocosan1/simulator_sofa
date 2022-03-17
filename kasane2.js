973feefcd5dfde1b15bf06ed3d415a1f6f76b842
hskw1 <isishskw@gmail.com>
2022-03-18 07:39:13 +0900

    1st commit

---
+++ kasane2.js
@@ -0,0 +1,175 @@
+let array = new Array();
+array[''] = new Array({cd: "", label: "張地を選択してください"});
+
+array["tif"] = new Array(
+    {cd:"", label: "張地を選択してください"},
+    {cd:"fabric/seiburu_bu3_15.png", label: "セイブルBU_15"},
+    {cd:"fabric/torento_be3_3040.png", label: "トレントBE_3040"},
+    {cd:"fabric/torento_ye3_3040.png", label: "トレントYE_3040"},
+    {cd:"fabric/torento_or3_3040.png", label: "トレントOR_3040"},
+    {cd:"fabric/torento_g3_3040.png", label: "トレントG_3040"},
+    {cd:"fabric/torento_iv3_3040.png", label: "トレントIV_3040"},
+    {cd:"fabric/torento_gr3_3040.png", label: "トレントGR_3040"},
+    {cd:"fabric/seiburu_lbu3_15.png", label: "セイブルLBU_15"},
+    {cd:"fabric/seiburu_dbr3_3040.png", label: "セイブルDBR_3040"},
+    {cd:"fabric/seiburu_be3_3040.png", label: "セイブルBE_3040"},
+    {cd:"fabric/seiburu_iv3_10.png", label: "セイブルIV_10"}, 
+    {cd:"fabric/seiburu_pu3_3040.png", label: "セイブルPU_3040"},
+);
+
+array["b"] = new Array(
+    {cd:"", label: "張地を選択してください"},
+    {cd:"fabric/supaisu_lgr3_3040.png", label: "スパイスLGR_3040"}, /*若干線あり */
+    {cd:"fabric/supaisu_g3_3040.png", label: "スパイスG_3040"},
+    {cd:"fabric/safirosu_lgr3_3040.png", label: "サフィロスLGR_3040"},
+    {cd:"fabric/safirosu_or3_15.png", label: "サフィロスOR_15"},
+    {cd:"fabric/safirosu_br3_3040.png", label: "サフィロスBR_3040"},
+    {cd:"fabric/safirosu_dgr3_3040.png", label: "サフィロスDGR_3040"},
+    {cd:"fabric/supaisu_lgr3_3040.png", label: "スパイスLGR_3040"},
+    {cd:"fabric/supaisu_g3_3050.png", label: "スパイスG_3050"},
+    {cd:"fabric/syaamu_re3_3040.png", label: "シャームRE_3040"}, /*若干柄大きい */
+    {cd:"fabric/syaamu_br3_3040.png", label: "シャームBR_3040"},
+    {cd:"fabric/syaamu_be3_3040.png", label: "シャームBE_3040"},
+    {cd:"fabric/syaamu_gr3_3040.png", label: "シャームGR_3040"},
+    {cd:"fabric/maika_br3_3040.png", label: "マイカBR_3040"},
+    {cd:"fabric/maika_g3_3040.png", label: "マイカG_3040"},
+    {cd:"fabric/maika_lgr3_3040.png", label: "マイカLGR_3040"},
+    {cd:"fabric/maika_re3_3040.png", label: "マイカRE_3040"},
+    {cd:"fabric/puraha_br3_3040.png", label: "プラハBR_3040"},
+    {cd:"fabric/puraha_bu3_3040.png", label: "プラハBU_3040"},
+    {cd:"fabric/puraha_gr3_3040.png", label: "プラハGR_3040"},
+    {cd:"fabric/puraha_or3_3040.png", label: "プラハOR_3040"},
+    {cd:"fabric/puraha_ye3_3040.png", label: "プラハYE_3040"},
+    {cd:"fabric/puraha_b3_3040.png", label: "プラハB_3040"},
+    {cd:"fabric/kureo_b3_3040.png", label: "クレオB_3040"},
+    {cd:"fabric/kureo_be3_3040.png", label: "クレオBE_3040"},
+    {cd:"fabric/kureo_br3_3040.png", label: "クレオBR_3040"},
+    {cd:"fabric/ruche_be3_3040.png", label: "ルーチェBE_3040"}, /**若干線あり */
+    {cd:"fabric/ruche_br3_3040.png", label: "ルーチェBR_3040"},
+    {cd:"fabric/ruche_g3_3040.png", label: "ルーチェG_3040"},
+    {cd:"fabric/ruche_pu3_3040.png", label: "ルーチェPU_3040"},
+    {cd:"fabric/dokku_pu3_25.png", label: "ドックPU25"},
+    {cd:"fabric/dokku_br3_25.png", label: "ドックBR25"},
+    {cd:"fabric/dokku_b3_25.png", label: "ドックB25"},
+    {cd:"fabric/dokku_re3_25.png", label: "ドックRE25"},
+    {cd:"fabric/dokku_yg3_25.png", label: "ドックYG25"},
+
+);
+
+array["c"] = new Array(
+    {cd:"", label: "張地を選択してください"},
+    {cd:"fabric/gaucho_lgr3_3040.png", label: "ガウチョLGR_3040"},
+    {cd:"fabric/gaucho_br3_3040.png", label: "ガウチョBR_3040"},
+    {cd:"fabric/gaucho_gr3_3040.png", label: "ガウチョGR_3040"},
+    {cd:"fabric/begasu_g3_3040.png", label: "ベガスG_3040"},
+    {cd:"fabric/begasu_b3_3050.png", label: "ベガスB_3050"},
+    {cd:"fabric/begasu_dgr3_3040.png", label: "ベガスDGR_3040"},
+    {cd:"fabric/begasu_gr3_3040.png", label: "ベガスGR_3040"},
+    {cd:"fabric/begasu_re3_3040.png", label: "ベガスRE_3040"},
+    {cd:"fabric/nohara_br3_3040.png", label: "ノハラBR_3040"},
+    {cd:"fabric/nohara_bu3_3040.png", label: "ノハラBU_3040"},
+    {cd:"fabric/supiritto_be3_3040.png", label: "スピリットBE_3040"},
+    {cd:"fabric/supiritto_br3_3040.png", label: "スピリットBR_3040"}, /*若干線あり */
+    {cd:"fabric/supiritto_bu3_3040.png", label: "スピリットBU_3040"}, /*若干線あり */
+    {cd:"fabric/supiritto_gr3_3040.png", label: "スピリットGR_3040"}, /*若干線あり */
+    {cd:"fabric/supiritto_pu3_3040.png", label: "スピリットPU_3040"},
+    {cd:"fabric/buruumu_bu3_3040.png", label: "ブルームBU_3040"},
+    {cd:"fabric/buruumu_g3_3040.png", label: "ブルームG_3040"},
+    {cd:"fabric/russo_be3_3040.png", label: "ルッソBE_3040"},
+    {cd:"fabric/russo_pu3_3040.png", label: "ルッソPU_3040"},
+    {cd:"fabric/russo_g3_3040.png", label: "ルッソG_3040"},
+    {cd:"fabric/daria_dbu3_3040.png", label: "ダリアDBU_3040"},
+    {cd:"fabric/daria_gr3_3040.png", label: "ダリアGR_3040"},
+    {cd:"fabric/daria_lgr3_3040.png", label: "ダリアLGR_3040"},
+    {cd:"fabric/guran_br3_3040.png", label: "グランBR_3040"}, /*若干線あり */
+    {cd:"fabric/guran_dbr3_3040.png", label: "グランDBR_3040"},
+    
+);
+
+array["e"] = new Array(
+    {cd:"", label: "張地を選択してください"},
+    {cd:"fabric/hauzen_gr3_3040.png", label: "ハウゼンGR_3040"},
+    {cd:"fabric/hauzen_nv3_3040.png", label: "ハウゼンNV_3040"},
+    {cd:"fabric/jupita_dbu3_3040.png", label: "ジュピターDBU_3040"},
+    {cd:"fabric/jupita_be3_3040.png", label: "ジュピターBE_3040"},
+    {cd:"fabric/aran_be3_3040.png", label: "アランBE_3040"},
+    {cd:"fabric/aran_dbu3_3040.png", label: "アランDBU_3040"},
+    {cd:"fabric/houmutuido_be3_3040.png", label: "ホームツイードBE_3040"},
+    {cd:"fabric/houmutuido_gr3_3040.png", label: "ホームツイードGR_3040"},
+    {cd:"fabric/torasu_be3_3040.png", label: "トラスBE_3040"},
+    {cd:"fabric/torasu_gr3_3040.png", label: "トラスGR_3040"},
+    {cd:"fabric/torasu_yg3_3040.png", label: "トラスYG_3040"},
+    
+    );
+
+array["gb"] = new Array(
+    {cd:"", label: "張地を選択してください"},
+    {cd:"fabric/hairarugo_br3_3040.png", label: "ハイラルゴBR_3040"},
+    {cd:"fabric/hairarugo_dbr3_3040.png", label: "ハイラルゴDBR_3040"},
+    {cd:"fabric/hairarugo_b3_3040.png", label: "ハイラルゴB_3040"},
+    {cd:"fabric/hairarugo_p3_3040.png", label: "ハイラルゴP_3040"},
+    {cd:"fabric/hairarugo_be3_3040.png", label: "ハイラルゴBE_3040"},
+    {cd:"fabric/hairarugo_ca3_3040.png", label: "ハイラルゴCA_3040"},
+    {cd:"fabric/hairarugo_bu3_3040.png", label: "ハイラルゴBU_3040"},
+    {cd:"fabric/hairarugo_lbu3_3040.png", label: "ハイラルゴLBU_3040"},
+    {cd:"fabric/hairarugo_lgr3_3040.png", label: "ハイラルゴLGR_3040"},
+    
+    );    
+
+array["ha"] = new Array(
+    {cd:"", label: "未収録"},
+    
+    );
+
+array["hb"] = new Array(
+    {cd:"", label: "未収録"},
+    
+    );
+
+array["s"] = new Array(
+    {cd:"", label: "張地を選択してください"},
+    {cd:"fabric/soho10_3_3040.png", label: "SOHO10_3040"},
+    {cd:"fabric/soho13_3_3040.png", label: "SOHO13_3040"},
+    {cd:"fabric/soho14_3_3040.png", label: "SOHO14_3040"},
+    {cd:"fabric/soho32_3_3040.png", label: "SOHO32_3040"},
+    {cd:"fabric/soho44_3_3040.png", label: "SOHO44_3040"},
+    {cd:"fabric/soho54_3_3040.png", label: "SOHO54_3040"},
+    {cd:"fabric/soho61_3_3040.png", label: "SOHO61_3040"},
+    {cd:"fabric/soho64_3_3040.png", label: "SOHO64_3040"},
+    {cd:"fabric/soho66_3_3040.png", label: "SOHO66_3040"},
+    {cd:"fabric/soho67_3_3040.png", label: "SOHO67_3040"},
+    {cd:"fabric/soho70_3_3040.png", label: "SOHO70_3040"},
+    {cd:"fabric/soho81_3_3040.png", label: "SOHO81_3040"},
+    {cd:"fabric/soho85_3_3040.png", label: "SOHO85_3040"},
+    {cd:"fabric/soho92_3_3040.png", label: "SOHO92_3040"},
+    
+    
+    );
+
+document.getElementById('fabric_rank').onchange = function() {
+    fabric_name = document.getElementById('fabric_name');
+    fabric_name.options.length = 0; //optionタグを空にする
+    let change_rank = fabric_rank.value;
+    for (let i = 0; i < array[change_rank].length; i++) {
+        let op = document.createElement("option"); //optionタグ生成
+        value = array[change_rank][i];　//arrayのcd　labelをvalueに投入
+        op.value = value.cd;
+        op.label = value.label;
+        fabric_name.appendChild(op);
+    }
+}
+
+function change_sofa(parts, id) {
+    //選択されているメニューを変数に格納
+    fname = parts.options[parts.selectedIndex].value;
+    //選択された画像のパスを送る
+    id.src = fname;
+}
+
+function change_fabric(parts, id) {
+    //選択されているメニューを変数に格納
+    fname = parts.options[parts.selectedIndex].value;
+    //選択された画像のパスを送る
+    id.src = fname;
+}
+
